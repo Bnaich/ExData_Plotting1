5dedc591880450efb33a3d246168d7f294489a35
Maxim Scherbakov <maxim.y.scherbakov@gmail.com>
2017-10-14 17:53:54 +0300

    Delete plot2.R

--- plot2.R
+++
@@ -1,4 +0,0 @@
-#plot 2
-par(mfrow = c(1,1))
-plot(dset$Dates, dset$Global_active_power, type = "n", xlab = "", ylab = "Global Active Power (kilowatts)")
-lines(dset$Dates, dset$Global_active_power, xlab = "", ylab = "Global Active Power (kilowatts)")
